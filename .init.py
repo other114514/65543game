4d3af25c6037886d4cba998ead50cb706deb4dac
other114514 <other_dog@163.com>
2024-05-21 20:44:44 +0800

    我喜欢你

--- .init.py
+++ .init.py
@@ -2,7 +2,7 @@ import os
 index='<meta charset="utf-8">'
 for i in os.listdir('./'):
     if not i.startswith("."):
-        index+=f"<a href='/{i}'>{i}</a></br>"
+        index+=f"<a href='/otherrepo.github.io/{i}'>{i}</a></br>"
 with open("index.html","w") as f:
     f.write(index)
 
