c88410315055d431d99d6fcdd34147abdf3117a6
other114514 <other_dog@163.com>
2024-05-21 20:40:56 +0800

    我喜欢你

--- .init.py
+++ .init.py
@@ -1,8 +1,8 @@
 import os
-index='<meta charset="utf-8>"'
+index='<meta charset="utf-8">'
 for i in os.listdir('./'):
     if not i.startswith("."):
-        index+=f"<a href=/{i}>{i}</a></br>"
+        index+=f"<a href='/{i}'>{i}</a></br>"
 with open("index.html","w") as f:
     f.write(index)
 
