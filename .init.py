a7bfeb2892b0b01052a59a81a30d164ddcfd4cbd
other114514 <other_dog@163.com>
2024-05-22 22:25:14 +0800

    我喜欢你

--- .init.py
+++ .init.py
@@ -1,9 +1,52 @@
 import os
-index='<meta charset="utf-8">'
-for i in os.listdir('./'):
-    if not i.startswith("."):
-        index+=f"<a href='/otherrepo.github.io/{i}'>{i}</a></br>"
-with open("index.html","w") as f:
-    f.write(index)
+index='''<meta charset="utf-8">
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>超链接网页</title>
+<style>
+  body {
+      margin: 0;
+          padding: 0;
+              font-family: Arial, sans-serif;
+                  background-color: #3498db; /* 蓝色背景 */
+                      color: #ffffff; /* 白色文字 */
+                        }
 
-         
+                          .container {
+                              display: flex;
+                                  flex-direction: column;
+                                      align-items: center;
+                                          justify-content: center;
+                                              height: 100vh;
+                                                  text-align: center;
+                                                    }
+
+                                                      .link {
+                                                          display: block;
+                                                              margin: 10px 0;
+                                                                  font-size: 24px;
+                                                                      font-weight: bold;
+                                                                          color: #ffffff; /* 白色文字 */
+                                                                              text-decoration: none; /* 去掉超链接下划线 */
+                                                                                }
+                                                                                </style>
+                                                                                </head>
+                                                                                <body>
+                                                                                <div class="container">'''
+def fu(d):
+ index_=index
+ for i in os.listdir(d):
+    if not i.startswith(".") and os.path.isfile(d+'/'+i) and i!='index.html':
+        index_+=f"<a href='/otherrepo.github.io{d[1:]}/{i}' class='link'>{i}</a></br>"
+    elif os.path.isdir(d+'/'+i) and not i.startswith('.'):   
+        index_+=f"<a href='/otherrepo.github.io{d[1:]}/{i}/index.html' class='link'>{i}</a></br>"
+        fu(d+'/'+i)
+ index_+='''</div>
+ </body>
+ </html>'''
+ with open(f"{d}/index.html","w")as f:
+     f.write(index_)
+fu('.')
